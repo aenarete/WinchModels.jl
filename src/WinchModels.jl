2c1e544b0a92645a90398cd9bee122e1b3abcff8
Uwe Fechner <fechner@aenarete.eu>
2022-05-13 23:42:09 +0200

    Add script plot_smooth_sign.jl

--- src/WinchModels.jl
+++ src/WinchModels.jl
@@ -25,7 +25,7 @@ using KiteUtils, Parameters
 
 export AsyncGenerator                                                      # types
 export calc_reactance, calc_inductance, calc_resistance                    # helper functions
-export calc_coulomb_friction, calc_viscous_friction                        # helper functions
+export calc_coulomb_friction, calc_viscous_friction, smooth_sign           # helper functions
 export calc_acceleration, calc_force                                       # main functions
 
 include("async_generator.jl")
---
+++ test/plot_smooth_sign.jl
@@ -0,0 +1,7 @@
+using Plots, WinchModels
+
+wm = deepcopy(AsyncGenerator())
+n = 256
+x = wm.gear_ratio/wm.drum_radius * range(-8.0, 8.0, length=n)
+s = smooth_sign.(x)
+plot(x,s)
