f0b3f082df0b50a43474c6d918d9415fa806e009
Uwe Fechner <fechner@aenarete.eu>
2024-07-22 11:46:00 +0200

    Improved plot

--- examples/plot_control_response.jl
+++ examples/plot_control_response.jl
@@ -4,11 +4,12 @@ if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
 end
 using ControlPlots
 
-v_wind = 12.1 # m/s
-β =      26.0 # degrees
-K =     123.0 # N/(m/s)^2
+v_ro =     3.0 # m/s
+β =       26.0 # degrees
+K =      123.0 # N/(m/s)^2
+period =  30.0 # seconds
 
-function force(v_ro)
+function force(v_wind)
     v_eff = v_wind*cosd(β) - v_ro
     if v_eff < 0
         return 0
@@ -16,5 +17,18 @@ function force(v_ro)
     K * (v_eff)^2
 end
 
+# implementation of a triangle wave generator
+function triangle_wave(t, period)
+    t = mod(t, period)
+    if t < period/2
+        return 2*t/period
+    else
+        return 2*(period-t)/period
+    end
+end
 
+time = range(0, 120, length=120)
+v_wind = 12.1 .+ 1.5*triangle_wave.(time, period)
+f = force.(v_wind)
 
+plot(time, f; xlabel="Time [s]", ylabel="Force")
