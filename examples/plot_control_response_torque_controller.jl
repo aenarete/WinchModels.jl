1a048c050bfaa7cffcd1d57969f0b1d91cf26c47
Uwe Fechner <fechner@aenarete.eu>
2024-07-24 12:53:36 +0200

    some progress, bump KiteUtils

--- examples/plot_control_response_torque_controller.jl
+++ examples/plot_control_response_torque_controller.jl
@@ -55,6 +55,33 @@ function simulate(t_sim=120; f_0=7663, speed_0=3.1735, dt=0.005)
     p2=plot(time, V_RO; xlabel="Time [s]", ylabel="Speed [m/s]", fig="speed")
     p3=plot(time, ACC; xlabel="Time [s]", ylabel="Acceleration [m/s^2]", fig="acceleration")
     display(p1); display(p2); display(p3)
+    F
+end
+
+function test(t_sim=120; f_0=7663, speed_0=3.1735, dt=0.005)
+    time = 0:dt:t_sim
+    F = Float64[]
+    ACC = Float64[]
+    V_RO = Float64[]
+    f = f_0
+    v_ro = speed_0
+    wm = TorqueControlledMachine()
+    for t in time
+        # calculate the set_speed using a ramp
+        # calculate the acceleration
+        acc = calc_acceleration(wm, v_ro, f; set_torque=7800, use_brake = false)
+        push!(ACC, acc)
+        # integrate the acceleration to get the velocity
+        # v_ro += acc*dt
+        push!(V_RO, v_ro)
+        f = calc_force(t, v_ro)
+        push!(F, f)
+    end
+    p1=plot(time, F; xlabel="Time [s]", ylabel="Force [N]", fig="force")
+    p2=plot(time, V_RO; xlabel="Time [s]", ylabel="Speed [m/s]", fig="speed")
+    p3=plot(time, ACC; xlabel="Time [s]", ylabel="Acceleration [m/s^2]", fig="acceleration")
+    display(p1); display(p2); display(p3)
+    F
 end
 
 simulate()
--- src/torque_controlled_generator.jl
+++ src/torque_controlled_generator.jl
@@ -101,14 +101,14 @@ function calc_acceleration(wm::TorqueControlledMachine, speed, force; set_torque
     wm.drum_radius/wm.gear_ratio * omega_dot_m
 end
 
-""" Calculate the tether force as function of the set_speed and speed. """
-function calc_force(wm::TorqueControlledMachine, set_speed, speed)
-    calc_force(wm::TorqueControlledMachine, speed; set_speed)
-end
+# """ Calculate the tether force as function of the set_speed and speed. """
+# function calc_force(wm::TorqueControlledMachine, set_speed, speed)
+#     calc_force(wm::TorqueControlledMachine, speed; set_speed)
+# end
 
-""" Calculate the tether force as function of the set_speed, set_torque and speed. """
-# TODO: fix the calculation of the force
-function calc_force(wm::TorqueControlledMachine, speed; set_speed=nothing, set_torque=nothing)
-    acc = calc_acceleration(wm, set_speed, speed, 0.0)
-    (wm.gear_ratio/wm.drum_radius) ^ 2 * wm.inertia_total * acc
-end
+# """ Calculate the tether force as function of the set_speed, set_torque and speed. """
+# # TODO: fix the calculation of the force
+# function calc_force(wm::TorqueControlledMachine, speed; set_speed=nothing, set_torque=nothing)
+#     acc = calc_acceleration(wm, set_speed, speed, 0.0)
+#     (wm.gear_ratio/wm.drum_radius) ^ 2 * wm.inertia_total * acc
+# end
